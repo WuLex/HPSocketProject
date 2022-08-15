b3efa5c545a19cd38973c52f331d229b19fb1fbe
wulex <827937686@qq.com>
2022-08-15 23:16:43 +0800

    初始化

---
+++ HPSocketDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using Autofac;
+using Autofac.Annotation;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+var serviceCollection = new ServiceCollection();
+
+//配置
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json");
+IConfiguration configuration = configBuilder.Build();
+serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+//日志
+serviceCollection.AddLogging(builder =>
+{
+    builder.AddConfiguration(configuration.GetSection("Logging")).AddConsole();
+});
+
+var builder = new ContainerBuilder();
+
+//注入服务器集合
+builder.Populate(serviceCollection);
+
+// 注册autofac打标签模式
+builder.RegisterModule(new AutofacAnnotationModule().SetAllowCircularDependencies(true));
+
+builder.RegisterBuildCallback(c =>
+{
+    var logger = c.Resolve<ILogger<Program>>();
+    logger.LogInformation("启动成功！");
+});
+
+builder.Build();
+
+while (true)
+{
+    Thread.Sleep(1000);
+}
---
+++ HPSocketDemo/SocketServer.cs
@@ -0,0 +1,52 @@
+﻿using Autofac.Annotation;
+using HPSocket;
+using HPSocket.Tcp;
+using Microsoft.Extensions.Logging;
+
+namespace HPSocketDemo
+{
+    [Component("SocketClient", AutofacScope = AutofacScope.SingleInstance, AutoActivate = true, InitMethod = nameof(SocketClient.Start))]
+    public class SocketClient
+    {
+        [Autowired]
+        private ILogger<SocketClient> logger;
+
+        public void Start()
+        {
+            ITcpClient tcpClient = new TcpClient
+            {
+                // 缓冲区大小,4kb
+                SocketBufferSize = 4096,
+                Async = true,
+                Address = "172.16.9.75",
+                Port = 5013,
+                KeepAliveInterval = 3000,
+                KeepAliveTime = 3000
+            };
+            tcpClient.Async = true;
+            tcpClient.Connect();
+            tcpClient.OnConnect += TcpClient_OnConnect; ;
+            tcpClient.OnClose += TcpClient_OnClose; ;
+            tcpClient.OnReceive += TcpClient_OnReceive; ;
+          
+        }
+
+        private HandleResult TcpClient_OnReceive(IClient sender, byte[] data)
+        {
+            logger.LogDebug("TcpClient_OnReceive");
+            return HandleResult.Ok;
+        }
+
+        private HandleResult TcpClient_OnClose(IClient sender, SocketOperation socketOperation, int errorCode)
+        {
+            logger.LogDebug($"TcpClient_OnClose#socketOperation:{socketOperation}");
+            return HandleResult.Ok;
+        }
+
+        private HandleResult TcpClient_OnConnect(IClient sender)
+        {
+            logger.LogDebug("TcpClient_OnConnect");
+            return HandleResult.Ok;
+        }
+    }
+}
