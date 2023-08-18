6007fe5426f661c6ea81cdea7602f32533ad6bd0
wulex <827937686@qq.com>
2023-08-19 01:17:57 +0800

    add game reconnect

---
+++ GameApp/GameClient.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameApp
+{
+    public class GameClient
+    {
+        public  bool isConnected = false;
+        private bool isPlaying = false;
+        private bool isDisconnected = false;
+
+        public void Connect()
+        {
+            // 模拟连接服务器
+            Console.WriteLine("连接服务器...");
+            Thread.Sleep(1000); // 模拟网络延迟
+
+            isConnected = true;
+            Console.WriteLine("连接成功！");
+        }
+
+        public void PlayGame()
+        {
+            // 模拟进行游戏
+            isPlaying = true;
+            Console.WriteLine("游戏开始...");
+
+            while (isPlaying)
+            {
+                // 模拟发送心跳包
+                SendHeartbeat();
+
+                // 模拟游戏过程
+                // ...
+
+                // 模拟断线
+                //if (!isConnected)
+                if (true)
+                {
+                    Disconnect();
+                    break;
+                }
+
+                Thread.Sleep(2000); // 模拟游戏过程中的时间延迟
+            }
+        }
+
+        public void Disconnect()
+        {
+            // 断开连接操作
+            isConnected = false;
+            isPlaying = false;
+            isDisconnected = true;
+
+            Console.WriteLine("与服务器断开连接！");
+        }
+
+        public void Reconnect()
+        {
+            if (isDisconnected)
+            {
+                // 尝试重新连接服务器
+                Connect();
+
+                if (isConnected)
+                {
+                    // 重新连接成功后恢复游戏状态
+                    RestoreGameState();
+                    ResumeGame();
+                }
+            }
+        }
+
+        private void SendHeartbeat()
+        {
+            // 发送心跳包给服务器
+            Console.WriteLine("发送心跳包...");
+            Thread.Sleep(500); // 模拟网络延迟
+        }
+
+        private void RestoreGameState()
+        {
+            // 恢复游戏状态的操作，例如获取之前保存的游戏状态数据
+            Console.WriteLine("恢复游戏状态...");
+            Thread.Sleep(1000); // 模拟网络延迟
+        }
+
+        private void ResumeGame()
+        {
+            // 恢复游戏继续进行
+            Console.WriteLine("继续游戏...");
+            isPlaying = true;
+        }
+    }
+}
---
+++ GameApp/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using GameApp;
+
+GameClient client = new GameClient();
+
+// 连接服务器并开始游戏
+client.Connect();
+if (client.isConnected)
+{
+    client.PlayGame();
+}
+
+// 模拟断线重连
+client.Reconnect();
+
+Console.ReadKey();
